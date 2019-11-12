74419570d8afffdbd84e58a5b9d59c5914453911
longhbph07396 <longhbph07396@fpt.edu.vn>
2019-11-12 18:13:06 +0700

    màn hình login

--- app/src/main/java/fpoly/com/duan1/M1_0_HelloActivity.java
+++ app/src/main/java/fpoly/com/duan1/M1_0_HelloActivity.java
@@ -2,6 +2,8 @@ package fpoly.com.duan1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -19,9 +21,23 @@ public class M1_0_HelloActivity extends AppCompatActivity {
 
         imgvIcon = (ImageView) findViewById(R.id.imgvIcon);
 
-        Animation animation= AnimationUtils.loadAnimation(this,R.anim.icon_xoay);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.icon_xoay);
         animation.setInterpolator(new LinearInterpolator());
         imgvIcon.startAnimation(animation);
 
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    startActivity(new Intent(M1_0_HelloActivity.this, M2_0_LoginActivity.class));
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                super.run();
+            }
+        };
+        thread.start();
     }
 }
--- app/src/main/java/fpoly/com/duan1/M2_0_LoginActivity.java
+++ app/src/main/java/fpoly/com/duan1/M2_0_LoginActivity.java
@@ -1,15 +1,41 @@
 package fpoly.com.duan1;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class M2_0_LoginActivity extends AppCompatActivity {
 
+    private ImageView imgvIconM20;
+    private LinearLayout lnlM20;
+    private LinearLayout lnlM201;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m2_0__login);
-        //aaa
+
+        imgvIconM20 = (ImageView) findViewById(R.id.imgvIconM20);
+        lnlM20 = (LinearLayout) findViewById(R.id.lnlM20);
+        lnlM201 = (LinearLayout) findViewById(R.id.lnlM201);
+
+//Xét các hiệu ứng animation khi vào màn hình
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.m20_icon);
+        animation.setInterpolator(new LinearInterpolator());
+        imgvIconM20.startAnimation(animation);
+
+
+        Animation animation1= AnimationUtils.loadAnimation(this,R.anim.m20_dangnhap);
+        animation1.setInterpolator(new LinearInterpolator());
+        lnlM20.startAnimation(animation1);
+
+        Animation animation2= AnimationUtils.loadAnimation(this,R.anim.m20_dangnhap0);
+        animation2.setInterpolator(new LinearInterpolator());
+        lnlM201.startAnimation(animation2);
+
     }
 }
