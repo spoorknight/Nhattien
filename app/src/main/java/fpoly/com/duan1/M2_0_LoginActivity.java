68c0c4c03017f6a31abd0b74d9f6a6e1941582ed
longhbph07396 <longhbph07396@fpt.edu.vn>
2019-11-12 13:26:10 +0700

    aa

--- app/src/main/java/fpoly/com/duan1/M2_0_LoginActivity.java
+++ app/src/main/java/fpoly/com/duan1/M2_0_LoginActivity.java
@@ -10,6 +10,6 @@ public class M2_0_LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m2_0__login);
-        //aa
+        //aaa
     }
 }
