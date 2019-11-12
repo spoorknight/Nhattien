7f1d80df0d41f8c9c95c4dd267e1f3208493bcfe
longhbph07396 <longhbph07396@fpt.edu.vn>
2019-11-12 13:00:07 +0700

    aa

---
+++ app/src/main/java/fpoly/com/duan1/bcdActtivity.java
@@ -0,0 +1,14 @@
+package fpoly.com.duan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bcdActtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bcd_acttivity);
+    }
+}
